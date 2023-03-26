7b4e255423565389b6f81fec5872e7f428cfda60
Rana Tabassum <rana.tabassum.10536@gmail.com>
2023-03-26 15:46:17 +0600

    lab commit

---
+++ lab3/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using lab3.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab3.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CatRegister()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CatRegister(Category model)
+        {
+
+            var db = new lab3Entities1();
+            db.Categorys.Add(model);
+
+            db.SaveChanges();
+            return RedirectToAction("CatList");
+        }
+        public ActionResult CatList()
+        {
+            var db = new lab3Entities1();
+            var categorys = db.Categorys.ToList();
+            return View(categorys);
+        }
+   
+
+/*public ActionResult Edit(int id)
+{
+    var db = new UMSSp23_CEntities1();
+    var ct = (from c in db.Categorys
+              where c.Id == id
+              select c).SingleOrDefault();
+    return View(ct);
+}
+[HttpPost]
+public ActionResult Edit(Category upcategory)
+{
+    var db = new UMSSp23_CEntities1();
+    var exst = (from c in db.Categorys
+                where c.Id == upcategory.Id
+                select c).SingleOrDefault();
+    exst.Name = upstudent.Name;
+    
+
+    //db.Entry(exst).CurrentValues.SetValues(upstudent);
+    db.SaveChanges();
+
+
+    //db.Students.Remove(exst);
+    return RedirectToAction("CatList");
+}*/
+    }
+}
---
+++ lab3/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using lab3.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab3.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register() { 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Product model)  {
+         
+            var db = new lab3Entities1();
+            db.Products.Add(model);
+            
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+        public ActionResult List() {
+            var db = new lab3Entities1();
+            var products = db.Products.ToList();
+            return View(products);
+        }
+        public ActionResult Edit(int id)
+        {
+            var db = new lab3Entities1();
+            var pr= (from p in db.Products
+                      where p.ID == id
+                      select p).SingleOrDefault();
+            return View(pr);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product upProduct)
+        {
+            var db = new lab3Entities1();
+            var exst = (from p in db.Products
+                        where p.ID == upProduct.ID
+                        select p).SingleOrDefault();
+            
+            exst.Name = upProduct.Name;
+            exst.Price = upProduct.Price;
+            exst.qty = upProduct.qty;
+
+
+            db.Entry(exst).CurrentValues.SetValues(upProduct);
+            db.SaveChanges();
+
+
+           
+            return RedirectToAction("List");
+        }
+        public ActionResult Remove(int id)
+        {
+            var db = new lab3Entities1();
+            var pr = (from p in db.Products
+                      where p.ID == id
+                      select p).SingleOrDefault();
+            return View(pr);
+        }
+        [HttpPost]
+        public ActionResult Remove(Product reProduct)
+        {
+            var db = new lab3Entities1();
+            var exst = (from p in db.Products
+                        where p.ID == reProduct.ID
+                        select p).SingleOrDefault();
+            exst.Name = reProduct.Name;
+
+
+
+            db.Entry(exst).CurrentValues.SetValues(reProduct);
+            db.SaveChanges();
+
+
+            db.Products.Remove(exst);
+            return RedirectToAction("List");
+        }
+
+
+    }
+}
